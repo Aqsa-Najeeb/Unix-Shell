f4cf998d170f44f506f6ec34075c4aca98d4bcfa
Aqsa Najeeb <jenscar777@gmail.com>
2026-08-04 18:34:32 +0500

    Created basic loop of a shell; lsh_loop

--- shell.c
+++ shell.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void lsh_loop(void)
+{
+    char * line;
+    char **args;
+    int status;
+
+    do 
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);  // without this a memory leak will occur
+        free(args);
+    } while(status);
+}
+
 int main(int argc, char **argv)
 {
-  
+  (void) argc;
+  (void) argv;
     // Load config files, if any.
 
     // Run command loop.

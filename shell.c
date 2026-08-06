2c264c379c868eb5117ff5c008decc3bafcc400f
Aqsa Najeeb <jenscar777@gmail.com>
2026-08-06 16:13:53 +0500

    Created a function to either launch a builtin or a process

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 
 #define LSH_TOK_BUFSIZE 64
@@ -163,6 +164,23 @@ int lsh_launch(char **args)
   }
 
   return 1;
+}
+
+int lsh_execute(char **args)
+{
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (int i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
 }
 
 void lsh_loop(void)
